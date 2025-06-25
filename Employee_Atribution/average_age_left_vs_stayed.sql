2d2f17f5e012265daad3e8123e1e3faf7a03a039
harsh-shende01 <shendeharsh.2001@gmail.com>
2025-06-25 08:38:03 +0530

    Delete Employee_Atribution/average_age_left_vs_stayed.sql

--- Employee_Atribution/average_age_left_vs_stayed.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    LeftCompany,
-    ROUND(AVG(Age), 1) AS AvgAge,
-    ROUND(AVG(Tenure), 2) AS AvgTenure,
-    ROUND(AVG(Salary), 0) AS AvgSalary
-FROM 
-    employee_attrition
-GROUP BY 
-    LeftCompany;
