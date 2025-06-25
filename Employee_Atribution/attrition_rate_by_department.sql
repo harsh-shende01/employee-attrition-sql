3ad1a367aa86a58decfcd1708d1e61cb0ba88ce0
harsh-shende01 <shendeharsh.2001@gmail.com>
2025-06-25 08:37:52 +0530

    Delete Employee_Atribution/attrition_rate_by_department.sql

--- Employee_Atribution/attrition_rate_by_department.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    Department,
-    COUNT(*) AS TotalEmployees,
-    SUM(LeftCompany = 'Yes') AS EmployeesLeft,
-    ROUND((SUM(LeftCompany = 'Yes') / COUNT(*)) * 100, 2) AS AttritionRate
-FROM 
-    employee_attrition
-GROUP BY 
-    Department;
